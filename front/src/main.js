64371490a97382f324e47dc56c7ba89c47b5cf01
Kirill <kirill.romanov@webideal.pro>
2020-09-13 17:15:24 +0400

    Add notifier

--- front/src/main.js
+++ front/src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import Notifications from 'vue-notification'
 import { BaseComponentsPlugin } from './components/base'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './styles/common.scss'
 
+Vue.use(Notifications)
 Vue.use(BaseComponentsPlugin)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
